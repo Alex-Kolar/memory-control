53185f6ead001205879fe2e81967cba515bba6b4
Alex-Kolar <alexkolar@comcast.net>
2025-02-19 12:24:04 -0600

    Latest version of afc_prep_hdawg_phase.py for microwave computer

--- afc_prep_hdawg_phase.py
+++ afc_prep_hdawg_phase.py
@@ -1,40 +1,50 @@
+"""Code to generate an AFC using the HDAWG and a phase EOM.
+
+BEFORE CODE CAN RUN SUCCESSFULLY:
+Upload and save the following code to the HDAWG
+
+const LENGTH = 4800000;                    // Replace with the length output from file
+wave w = placeholder(LENGTH, true, false); // Create a waveform of size LENGTH, with one marker
+assignWaveIndex(1, w, 10);                 // Create a wave table entry with placeholder waveform
+                                           // routed to output 1, with index 10
+playWave(1, w);
+"""
+
 from zhinst.toolkit import Session
 from zhinst.toolkit import Waveforms
 import numpy as np
 
 from afc_prep import full_waveform
-from afc_prep import full_waveform_noshift
 
 
 if __name__ == '__main__':
     # device params
     DEVICE_ID = 'DEV8345'
     SERVER_HOST = 'localhost'
-    samp_rate = 2.4e9  # unit: Hz
-    volt_range = 5.0  # unit: V
+    samp_rate = 2.4e9  # (Hz)
+    volt_range = 5.0  # (V)
 
     # EOM params
-    Vpi = 3.9  # unit: V
+    Vpi = 3.9  # (V)
 
     # waveform params
     A = Vpi / (volt_range * np.pi)  # overall amplitude of pulse (after normalization)
-    N = 1
-    delta = 1e6  # unit: Hz
-    tau = 2e-3  # unit: s
-    beta = 10 / tau
-    delta_f = 0.7e6  # unit: Hz
-    f_0 = 100e6  # unit: Hz
-    f_light = 195e12  # light frequency (in Hz)
+    N = 99
+    delta = 1e6  # (Hz)
+    tau = 2e-3  # (s)
+    beta = 10 / tau  # (Hz)
+    delta_f = 0.7e6  # (Hz)
+    f_0 = 100e6  # center frequency (Hz)
+    f_light = 195e12  # light frequency (Hz)
 
-    resolution = 1 / samp_rate  # unit: s
+    # calculation of waveform
+    resolution = 1 / samp_rate  # (s)
     num_points = tau / resolution
     num_points = (num_points // 16) * 16  # round to multiple of 16
 
     t, theta, amp = (
         full_waveform(N, delta, num_points, resolution, beta, f_light, delta_f))
-    # t, theta, amp = (
-    #     full_waveform_noshift(N, delta, num_points, resolution, beta, 0, delta_f))
-    coeff = amp / (volt_range * np.max(amp))
+    coeff = amp / (volt_range * np.max(amp))  # maximum modulation when max voltage is about 1
     wav = coeff * np.sin(2*np.pi*f_0*t + theta)  # V(t)
     print(f"len: {len(wav)}")
 
@@ -42,7 +52,7 @@ if __name__ == '__main__':
     session = Session(SERVER_HOST)
     device = session.connect_device(DEVICE_ID)
 
-    # define waveform
+    # define waveform marker
     marker = np.ones_like(wav, dtype=int)
     marker[int(len(wav)/2):] = 0
 
